dc9627fd637ae1b0ae3fc497cfea68bf6fb7e345
Farhad Safarov <farhad.safarov@gmail.com>
2019-11-12 22:08:12 +0300

    repository string shortcut issue

--- src/Handler/ClassHandler.php
+++ src/Handler/ClassHandler.php
@@ -3,17 +3,25 @@
 namespace Seferov\SymfonyPsalmPlugin\Handler;
 
 use PhpParser\Node;
+use PhpParser\Node\Expr;
+use PhpParser\Node\Expr\ClassConstFetch;
+use PhpParser\Node\Expr\MethodCall;
+use PhpParser\Node\Expr\StaticCall;
 use Psalm\Codebase;
 use Psalm\CodeLocation;
+use Psalm\Context;
 use Psalm\FileManipulation;
 use Psalm\IssueBuffer;
 use Psalm\Plugin\Hook\AfterClassLikeAnalysisInterface;
+use Psalm\Plugin\Hook\AfterMethodCallAnalysisInterface;
 use Psalm\StatementsSource;
 use Psalm\Storage\ClassLikeStorage;
+use Psalm\Type\Union;
 use Seferov\SymfonyPsalmPlugin\Issue\ContainerDependency;
+use Seferov\SymfonyPsalmPlugin\Issue\RepositoryStringShortcut;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
-class ClassHandler implements AfterClassLikeAnalysisInterface
+class ClassHandler implements AfterClassLikeAnalysisInterface, AfterMethodCallAnalysisInterface
 {
     /**
      * Called after a statement has been checked
@@ -38,5 +46,39 @@ class ClassHandler implements AfterClassLikeAnalysisInterface
         }
 
         return null;
+    }
+
+    /**
+     * @param MethodCall|StaticCall $expr
+     * @param FileManipulation[] $file_replacements
+     *
+     * @return void
+     */
+    public static function afterMethodCallAnalysis(
+        Expr $expr,
+        string $method_id,
+        string $appearing_method_id,
+        string $declaring_method_id,
+        Context $context,
+        StatementsSource $statements_source,
+        Codebase $codebase,
+        array &$file_replacements = [],
+        Union &$return_type_candidate = null
+    ) {
+        if (!$expr instanceof MethodCall) {
+            return;
+        }
+
+        if ($expr->name instanceof Node\Identifier && 'getRepository' === $expr->name->name) {
+            /** @psalm-suppress PossiblyNullReference */
+            foreach ($expr->var->inferredType->getTypes() as $className => $type) {
+                if ('Doctrine\ORM\EntityManagerInterface' === $className && !$expr->args[0]->value instanceof ClassConstFetch) {
+                    IssueBuffer::accepts(
+                        new RepositoryStringShortcut(new CodeLocation($statements_source, $expr->args[0]->value)),
+                        $statements_source->getSuppressedIssues()
+                    );
+                }
+            }
+        }
     }
 }
--- src/Plugin.php
+++ src/Plugin.php
@@ -12,7 +12,7 @@ class Plugin implements PluginEntryPointInterface
     /** @return void */
     public function __invoke(RegistrationInterface $api, SimpleXMLElement $config = null)
     {
-        require_once __DIR__ . '/Handler/ClassHandler.php';
+        require_once __DIR__.'/Handler/ClassHandler.php';
 
         $api->registerHooksFromClass(ClassHandler::class);
     }
