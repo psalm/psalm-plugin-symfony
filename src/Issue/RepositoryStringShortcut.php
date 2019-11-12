7002339f2ad822429048b44cc621eb773d266e61
Farhad Safarov <farhad.safarov@gmail.com>
2019-11-12 22:16:37 +0300

    repository string shortcut issue (#2)

---
+++ src/Issue/RepositoryStringShortcut.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Seferov\SymfonyPsalmPlugin\Issue;
+
+use Psalm\CodeLocation;
+use Psalm\Issue\CodeIssue;
+
+class RepositoryStringShortcut extends CodeIssue
+{
+    public function __construct(CodeLocation $code_location)
+    {
+        parent::__construct('Use Entity::class syntax instead', $code_location);
+    }
+}
