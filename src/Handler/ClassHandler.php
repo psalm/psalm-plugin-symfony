9bb33f551fd34935070b8200f7b2a2b406639681
Patrizia Wacht <patrizia@tideways-gmbh.com>
2020-03-04 20:25:38 +0100

    load with $config ContaimerServicefile, parse service<=>class

--- src/Handler/ClassHandler.php
+++ src/Handler/ClassHandler.php
@@ -63,6 +63,20 @@ class ClassHandler implements AfterClassLikeAnalysisInterface, AfterMethodCallAn
                     $className = (string) $expr->args[0]->value->class->getAttribute('resolvedName');
                     $return_type_candidate = new Union([new TNamedObject($className)]);
                 }
+                $classMapFromServiceContainer = self::loadServiceFile($codebase);
+                if ($return_type_candidate && count($classMapFromServiceContainer) && $expr->args[0]->value instanceof Node\Scalar\String_) {
+                    $serviceName = $expr->args[0]->value->value;
+                    if ($serviceName) {
+                        for ($a=0; $a<=count($classMapFromServiceContainer); $a++) {
+                            /** @psalm-suppress MixedArrayAccess */
+                            if($serviceName === $classMapFromServiceContainer[$a]['service']) {
+                                /** @psalm-suppress MixedArrayAccess */
+                                $return_type_candidate = new Union([new TNamedObject($classMapFromServiceContainer[$a]['class'])]);
+                            }
+                        }
+                    }
+                }
+
                 break;
             case 'Symfony\Component\HttpFoundation\Request::getcontent':
                 if ($return_type_candidate) {
@@ -85,4 +99,29 @@ class ClassHandler implements AfterClassLikeAnalysisInterface, AfterMethodCallAn
                 break;
         }
     }
+
+    private static function loadServiceFile(Codebase $codebase) : array {
+
+        $classServiceMap = [];
+        $simpleXmlConfig = $codebase->config->getPluginClasses()[0]['config'];
+        if (isset($simpleXmlConfig)) {
+            $serviceFiles = $simpleXmlConfig->children();
+            foreach($serviceFiles as $serviceFile) {
+                $serviceFilePath = (string)$serviceFile;
+                if (file_exists($serviceFilePath)) {
+                    $xml = simplexml_load_file($serviceFilePath);
+                    foreach($xml->services->service as $serviceObj) {
+                        $serviceAttributes = $serviceObj ? $serviceObj->attributes() : null;
+                        if ($serviceAttributes && $serviceAttributes['id'] && $serviceAttributes['class']) {
+                            $classServiceMap[] = ['service' => (string)$serviceAttributes['id'], 'class' => (string) $serviceAttributes['class']];
+                        }
+                    }
+                }
+
+            }
+        }
+        return $classServiceMap;
+    }
+
+
 }
