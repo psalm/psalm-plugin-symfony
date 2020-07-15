32ad6bf69d398b87439377fe4f65d6e1c7ac7d3e
Adrien LUCAS <adrien.lucas@smile.fr>
2020-07-15 06:43:51 +0200

    Fix type resolving

--- src/Taint/RequestTaint.php
+++ src/Taint/RequestTaint.php
@@ -6,48 +6,44 @@ namespace Psalm\SymfonyPsalmPlugin\Taint;
 
 
 use PhpParser\Node\Expr;
+use PhpParser\Node\Expr\MethodCall;
+use PhpParser\Node\Scalar\String_;
 use Psalm\Codebase;
 use Psalm\CodeLocation;
 use Psalm\Context;
-use Psalm\FileManipulation;
-use Psalm\Internal\Analyzer\Statements\Expression\ExpressionIdentifier;
-use Psalm\Issue\TaintedInput;
-use Psalm\IssueBuffer;
 use Psalm\Plugin\Hook\AfterExpressionAnalysisInterface;
 use Psalm\StatementsSource;
-use PhpParser\PrettyPrinter\Standard;
 use Psalm\Type\TaintKindGroup;
-use RuntimeException;
 use Symfony\Component\HttpFoundation\HeaderBag;
-use Symfony\Component\HttpFoundation\ParameterBag;
 
 class RequestTaint implements AfterExpressionAnalysisInterface
 {
 
-    public static function afterExpressionAnalysis(Expr $expr, Context $context, StatementsSource $statementsSource, Codebase $codebase, array &$fileReplacements = [])
+    public static function afterExpressionAnalysis(Expr $expr, Context $context, StatementsSource $statementsSource, Codebase $codebase, array &$fileReplacements = []): void
     {
-        if(!$expr instanceof Expr\MethodCall || strval($expr->name) !== 'get') {
+        if(!$expr instanceof MethodCall || strval($expr->name) !== 'get' || !isset($expr->args[0])) {
             return;
         }
 
-        $contextVariableName = ExpressionIdentifier::getVarId($expr->var, null);
-        if(!array_key_exists($contextVariableName, $context->vars_in_scope)) {
-            throw new RuntimeException(sprintf('Type of %s can not be determined because it was not found in context.', $contextVariableName));
-        }
-
-        $types = array_keys($context->vars_in_scope[$contextVariableName]->getAtomicTypes());
-        if(!in_array(HeaderBag::class, $types)) {
+        $firstArgument = $expr->args[0]->value;
+        $sourceType = $statementsSource->getNodeTypeProvider()->getType($expr);
+        if(!$firstArgument instanceof String_ || $sourceType === null) {
             return;
         }
 
-        $headerName = strtolower($expr->args[0]->value->value);
+        $types = $statementsSource->getNodeTypeProvider()->getType($expr->var);
+        if($types === null || !in_array(HeaderBag::class, array_keys($types->getAtomicTypes()))) {
+            return;
+        }
+
+        $headerName = strtolower($firstArgument->value);
         if($headerName !== 'user-agent') {
             return;
         }
 
         $uniqId = $statementsSource->getFileName() . ':' . $expr->getLine() . '/' . $expr->getStartTokenPos();
         $codebase->addTaintSource(
-            $statementsSource->getNodeTypeProvider()->getType($expr),
+            $sourceType,
             'tainted-header-'.$uniqId,
             TaintKindGroup::ALL_INPUT,
             new CodeLocation($statementsSource, $expr)
