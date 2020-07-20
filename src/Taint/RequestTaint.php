3f6bed7a09c93e70af2afad4838d9cfa21ef4da7
Farhad Safarov <farhad.safarov@gmail.com>
2020-07-20 07:18:23 +0300

    no message

--- src/Taint/RequestTaint.php
+++ src/Taint/RequestTaint.php
@@ -4,23 +4,17 @@ declare(strict_types=1);
 
 namespace Psalm\SymfonyPsalmPlugin\Taint;
 
-
 use PhpParser\Node\Expr;
-use PhpParser\Node\Expr\MethodCall;
-use PhpParser\Node\Expr\StaticCall;
 use PhpParser\Node\Scalar\String_;
 use Psalm\Codebase;
 use Psalm\CodeLocation;
 use Psalm\Context;
-use Psalm\FileManipulation;
 use Psalm\Plugin\Hook\AfterExpressionAnalysisInterface;
 use Psalm\Plugin\Hook\AfterMethodCallAnalysisInterface;
 use Psalm\StatementsSource;
 use Psalm\Type\TaintKindGroup;
 use Psalm\Type\Union;
-use RuntimeException;
 use Symfony\Component\HttpFoundation\HeaderBag;
-
 
 /*
  * The `afterMethodCallAnalysis` is used to spot the tainted method calls, while the
@@ -29,22 +23,24 @@ use Symfony\Component\HttpFoundation\HeaderBag;
  */
 class RequestTaint implements AfterMethodCallAnalysisInterface, AfterExpressionAnalysisInterface
 {
-    /** @var Expr */
+    /**
+     * @var Expr
+     */
     private static $nextTaintedNode;
 
     public static function afterMethodCallAnalysis(Expr $expr, string $method_id, string $appearing_method_id, string $declaring_method_id, Context $context, StatementsSource $statementsSource, Codebase $codebase, array &$file_replacements = [], Union &$return_type_candidate = null): void
     {
-        if($method_id !== HeaderBag::class.'::get') {
+        if ($method_id !== HeaderBag::class.'::get') {
             return;
         }
 
         $firstArgument = $expr->args[0]->value;
-        if(!$firstArgument instanceof String_) {
+        if (!$firstArgument instanceof String_) {
             return;
         }
 
         $headerName = strtolower($firstArgument->value);
-        if($headerName !== 'user-agent') {
+        if ('user-agent' !== $headerName) {
             return;
         }
 
@@ -59,14 +55,14 @@ class RequestTaint implements AfterMethodCallAnalysisInterface, AfterExpressionA
         static::$nextTaintedNode = null;
 
         $type = $statementsSource->getNodeTypeProvider()->getType($expr);
-        if ($type === null) {
+        if (null === $type) {
             throw new \RuntimeException('Can not guess the type of the expression.');
         }
 
-        $uniqId = $statementsSource->getFileName() . ':' . $expr->getLine() . '/' . $expr->getStartTokenPos();
+        $uniqId = $statementsSource->getFileName().':'.$expr->getLine().'/'.$expr->getStartTokenPos();
         $codebase->addTaintSource(
             $type,
-            'tainted-' . $uniqId,
+            'tainted-'.$uniqId,
             TaintKindGroup::ALL_INPUT,
             new CodeLocation($statementsSource, $expr)
         );
