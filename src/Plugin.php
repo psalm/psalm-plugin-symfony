dc27963c496f4bd23e51d1d361519bb7b6715c78
Adrien LUCAS <adrien.lucas@smile.fr>
2020-07-22 14:49:57 +0200

    Taint HeaderBag using the MethodReturnTypeProviderInterface

--- src/Plugin.php
+++ src/Plugin.php
@@ -12,7 +12,6 @@ use Psalm\SymfonyPsalmPlugin\Handler\ContainerHandler;
 use Psalm\SymfonyPsalmPlugin\Handler\DoctrineRepositoryHandler;
 use Psalm\SymfonyPsalmPlugin\Handler\HeaderBagHandler;
 use Psalm\SymfonyPsalmPlugin\Symfony\ContainerMeta;
-use Psalm\SymfonyPsalmPlugin\Taint\RequestTaint;
 use SimpleXMLElement;
 use Symfony\Component\HttpKernel\Kernel;
 
@@ -78,8 +77,5 @@ class Plugin implements PluginEntryPointInterface
         foreach ($stubs as $stubFilePath) {
             $api->addStubFile($stubFilePath);
         }
-
-        require_once __DIR__.'/Taint/RequestTaint.php';
-        $api->registerHooksFromClass(RequestTaint::class);
     }
 }
