2767346631826dd9047352e439fac35ed0546457
Adrien LUCAS <adrien.lucas@smile.fr>
2020-09-14 09:49:14 +0200

    Use the assert-package-constraint step provided by third party

--- src/Test/CodeceptionModule.php
+++ src/Test/CodeceptionModule.php
@@ -5,12 +5,7 @@ declare(strict_types=1);
 namespace Psalm\SymfonyPsalmPlugin\Test;
 
 use Codeception\Module as BaseModule;
-use Composer\InstalledVersions;
-use Composer\Semver\VersionParser;
 use InvalidArgumentException;
-use PackageVersions\Versions;
-use PHPUnit\Framework\SkippedTestError;
-use RuntimeException;
 use Twig\Cache\FilesystemCache;
 use Twig\Environment;
 use Twig\Extension\AbstractExtension;
@@ -59,33 +54,6 @@ class CodeceptionModule extends BaseModule
 
         $twigEnvironment = self::getEnvironment($rootDirectory, $cacheDirectory);
         $twigEnvironment->load($templateName);
-    }
-
-    /**
-     * @Given I have the :package package satisfying the :versionConstraint
-     */
-    public function haveADependencySatisfied(string $package, string $versionConstraint): void
-    {
-        $parser = new VersionParser();
-        /** @psalm-suppress UndefinedClass Composer\InstalledVersions is undefined when using Composer 1.x */
-        /** @psalm-suppress DeprecatedClass PackageVersions\Versions is used for Composer 1.x compatibility */
-        if (class_exists(InstalledVersions::class)) {
-            /** @var bool $isSatisfied */
-            $isSatisfied = InstalledVersions::satisfies($parser, $package, $versionConstraint);
-        } elseif (class_exists(Versions::class)) {
-            $version = (string) Versions::getVersion($package);
-
-            if (false === strpos($version, '@')) {
-                throw new RuntimeException('$version must contain @');
-            }
-
-            $isSatisfied = $parser->parseConstraints(explode('@', $version)[0])
-                ->matches($parser->parseConstraints($versionConstraint));
-        }
-
-        if (!isset($isSatisfied) || !$isSatisfied) {
-            throw new SkippedTestError("This scenario requires $package to match $versionConstraint");
-        }
     }
 
     private static function getEnvironment(string $rootDirectory, string $cacheDirectory): Environment
